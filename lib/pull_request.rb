75ec8d397b0e7cfe88772dd003a4d388dedb295e
Jesse Shawl <jesse@jshawl.com>
2015-02-17 08:42:37 -0500

    add tests

--- lib/pull_request.rb
+++ lib/pull_request.rb
@@ -1,15 +1,29 @@
 class PullRequest
-  attr_accessor :title
+  attr_accessor :title, :description, :errors
   def initialize
     @title = 'w03d02'
+    @description = ''
   end
   def valid_title?
     true if @title.match(/w[0-9]{2}d[0-9]{2}/)
+  end
+  def comfort?
+    true if @description.match(/comfort: [0-5]/)
+  end
+  def completeness?
+    true if @description.match(/completeness: ?[0-5]/)
   end
   def errors?
     @errors = []
     unless valid_title?
       @errors << "Title must be in w##d## format."
     end
+    unless comfort?
+      @errors << "Description must contain comfort value."
+    end
+    unless completeness?
+      @errors << "Description must contain completeness value."
+    end
+    @errors
   end
 end
--- spec/pull_request_spec.rb
+++ spec/pull_request_spec.rb
@@ -20,4 +20,16 @@ describe PullRequest do
     @pr.title = "w02D03"
     expect(@pr.errors?).to_not eq(nil)
   end
+  it "should have a comfort value" do
+    @pr = PullRequest.new
+    @pr.description = "comfort: 3"
+    expect(@pr.comfort?).to eq(true)
+  end
+  it "should have a completeness value" do
+    @pr = PullRequest.new
+    @pr.description = "comfort: 3\n completeness:3"
+    expect(@pr.completeness?).to eq(true)
+    @pr.description = "comfort: 3\n :3"
+    expect(@pr.completeness?).to eq(nil)
+  end
 end
