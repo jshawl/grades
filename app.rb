3aa08fc294ee121de9a0fc63d9668023181ddbc7
Jesse Shawl <jesse@jshawl.com>
2015-02-17 12:16:25 -0500

    add

--- app.rb
+++ app.rb
@@ -4,20 +4,22 @@ require 'json'
 require_relative 'lib/pull_request'
 
 post '/' do
-  id = JSON.parse( request.body.read )['number']
-  @pr = PullRequest.new( id )
-  if @pr.errors?
-    data = {
-      state:'closed'
-    }
-    res = HTTParty.patch('https://api.github.com/repos/jshawl/grades/pulls/' + @pr.id.to_s + '?access_token=' + ENV['access_token'], {
-      body: data.to_json
-    })
-    comment = {
-      body: @pr.errors?.join("\n\n")
-    }
-    res = HTTParty.post('https://api.github.com/repos/jshawl/grades/issues/' + @pr.id.to_s + '/comments?access_token=' + ENV['access_token'], {
-      body: comment.to_json
-    })
+  res = JSON.parse( request.body.read )
+  if res['action'] == 'opened'
+    @pr = PullRequest.new( res['number'] )
+    if @pr.errors?
+      data = {
+	state:'closed'
+      }
+      res = HTTParty.patch('https://api.github.com/repos/jshawl/grades/pulls/' + @pr.id.to_s + '?access_token=' + ENV['access_token'], {
+	body: data.to_json
+      })
+      comment = {
+	body: @pr.errors?.join("\n\n")
+      }
+      res = HTTParty.post('https://api.github.com/repos/jshawl/grades/issues/' + @pr.id.to_s + '/comments?access_token=' + ENV['access_token'], {
+	body: comment.to_json
+      })
+    end
   end
 end
