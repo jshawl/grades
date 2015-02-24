273eb4bada901fd31785ad787bcc64b7959a8698
Adam Bray <adam.bray@gmail.com>
2015-02-24 17:42:39 -0500

    refactored invalid title context to share http stubs

--- spec/requests/app_spec.rb
+++ spec/requests/app_spec.rb
@@ -11,14 +11,16 @@ RSpec.describe "HW App" do
   let(:valid_pr_data) { {"title" => "w01d01", "body" => "comfort: 4 \ncompleteness: 3", "user" => {"login" => "pr_test"} }}
 
   context "PR has no title" do
-    it "should post a comment stating 'invalid title' to the PR" do
+    before(:each) do
       data_without_title = valid_pr_data.merge("title" => '')
       # first request from pull_request.rb
       stub_request(:any, "https://api.github.com/repos/#{ENV['org_repo']}/pulls/#{pr_id}?access_token=#{ENV['github_access_token']}").
         to_return( body: data_without_title.to_json )
       stub_request(:any, /api.github.com.*commits/).to_return( body: {}.to_json )
       stub_request(:any, /api.github.com.*issues.*comments/)
+    end
 
+    it "should post a comment stating 'invalid title' to the PR" do
       # simulates github webhook
       post "/", { "action" => "opened", "number" => pr_id }.to_json
 
