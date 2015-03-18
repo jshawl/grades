4700be961c4899a55c74ea28b37cdc1dab21408e
Jesse Shawl <jesse@jshawl.com>
2015-03-18 07:20:09 -0400

    update repo name

--- app.rb
+++ app.rb
@@ -12,13 +12,13 @@ post '/' do
       data = {
 	state:'closed'
       }
-      res = HTTParty.patch('https://api.github.com/repos/jshawl/hw/pulls/' + @pr.id.to_s + '?access_token=' + ENV['access_token'], {
+      res = HTTParty.patch('https://api.github.com/repos/ga-students/addbass-hw/pulls/' + @pr.id.to_s + '?access_token=' + ENV['access_token'], {
 	body: data.to_json
       })
       comment = {
 	body: @pr.errors?.join("\n\n")
       }
-      res = HTTParty.post('https://api.github.com/repos/jshawl/hw/issues/' + @pr.id.to_s + '/comments?access_token=' + ENV['access_token'], {
+      res = HTTParty.post('https://api.github.com/repos/ga-students/addbass-hw/issues/' + @pr.id.to_s + '/comments?access_token=' + ENV['access_token'], {
 	body: comment.to_json
       })
     end
